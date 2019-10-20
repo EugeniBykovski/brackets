d61a52aa1e226aca37567e7eb6dead4fc5cbb510
EugeniBykovski <bykovskieugeni@mail.ru>
2019-10-20 11:40:26 +0300

    this my brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var stack = [];
+    bracketsConfig = {
+        '[': ']',
+        '{': '}',
+        '(': ')'
+    };
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "(" || str[i] == "{" || str[i] == "[") {
+            stack.push(str[i]);
+        } else if (str[i] == ")") {
+            if (stack.pop() != "(") {
+                return false
+            }
+        } else if (str[i] == "}") {
+            if (stack.pop() != "{") {
+                return false
+            }
+        } else if (str[i] == "]") {
+            if (stack.pop() != "[") {
+                return false
+            }
+        }
+    }
+    return stack.length == 0;
 }
